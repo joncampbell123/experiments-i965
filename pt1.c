a435996108e4929aa55aaeaa3b84bcd74a966b33
jon <jon@5c8a68ae-0764-0410-9a4d-e5041eecefdb>
2009-04-24 00:59:24 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/tvbox/experiments-i965@32 5c8a68ae-0764-0410-9a4d-e5041eecefdb

--- pt1.c
+++ pt1.c
@@ -21,7 +21,11 @@
 #include "util.h"
 #include "mmap.h"
 
+#include <asm/mtrr.h>
+
 const int seizure_mode = 0;
+
+int mtrr_fd = -1;
 
 int GTT_PAGES = 0;
 uint32_t GTT_OFFSET = 0;
@@ -59,10 +63,6 @@ uint32_t ptr_to_fb(volatile void *x) {
 /* a buffer for the Graphics chip to blit from.
  * we own it in CPU space, and if programmed correctly, the graphics chipset will blit from it perfectly */
 
-void disable_gtt() {
-	MMIO(0x2020) = 0;
-}
-
 int main() {
 	iopl(3);
 
@@ -71,34 +71,106 @@ int main() {
 	if (!map_intel_resources())
 		return 1;
 
-	return 1;
-
-	disable_gtt();
-
-	/* and pick the 4MB mark for the GTT */
-	GTT_OFFSET = (4UL << 20UL);
-	GTT = (volatile uint32_t*)(fb_base + GTT_OFFSET);
-	GTT_PAGES = (2UL << 20UL) >> 2UL;
-	GTT_FENCE = GTT + GTT_PAGES;	/* and we choose 2MB GTT */
-	/* make default map: first 8MB is mapped directly */
-	{
-		unsigned int page = 0;
-		for (page=0;page < GTT_PAGES;page++)
-			GTT[page] = ((page << 12UL) + GTT_OFFSET + fb_base_vis) | (0 << 1) | 1;	/* 1:1 mapping of pages, main memory (meaning graphics), valid */
-	}
-	/* set it */
-	{
-		uint32_t align = (GTT_PAGES << 2UL);
-		if (align & (align - 1) == 0) { printf("0x%08X not power of 2\n",align); return 1; }
-		uint32_t ofs = GTT_OFFSET;
-		if ((ofs & align) != 0) { printf("0x%08X not aligned\n",ofs); return 1; }
-		if ((ofs & 0xFFF) != 0) { printf("0x%08X not page aligned\n",ofs); return 1; }
-		MMIO(0x2020) = (ofs + fb_base_vis) | (4 << 1) | 1;	/* offset, 2MB GTT, enabled */
+	if ((mtrr_fd = open("/proc/mtrr",O_RDWR)) < 0) {
+		printf("Cannot open MTRRs\n");
+		return 1;
 	}
 
-	usleep(500000);
-	disable_gtt();
-	return 1;
+	/* TODO: ask an intel chipset where the actual top of memory is,
+	 *       or else guess from Linux /proc/mem and round up. */
+	uint32_t top_of_memory = (512U << 20U);
+	printf("top of memory = 0x%08X\n",top_of_memory);
+	/* we can assume this for now for most Intel chipsets: the TSEG area (1MB) */
+	uint32_t TSEG = top_of_memory - 0x100000;
+	printf("TSEG = 0x%08X\n",TSEG);
+	/* TODO: ask the chipset what the "framebuffer" size is (meaning how large the "stolen" area is) */
+	uint32_t stolen = (8U << 20U);
+	printf("Stolen = 0x%08X\n",stolen);
+	uint32_t framebuffer_addr = TSEG - stolen;
+	printf("framebuffer (DRAM physical) = 0x%08X\n",framebuffer_addr);
+
+	/* conveniently, we can also assume Intel 855GM VESA BIOS behavior of sticking the page tables IT made at the
+	 * end of the framebuffer area */
+	/* TODO: get the page tables size */
+	uint32_t page_tables_size = 128U << 10U;
+	uint32_t vesa_bios_page_tables = framebuffer_addr + stolen - page_tables_size;
+	printf("VESA BIOS page tables = 0x%08X\n",vesa_bios_page_tables);
+
+	/* in case of messed-up tables, we do some very careful stepping around to restore sanity.
+	 * first: we mmap() 4K of memory and mlock it, make that just enough of a table to cover
+	 * maybe the first 4MB mapping (and we HOPE that nobody else attempts to use it!) */
+	int page;
+	volatile uint32_t *step1 = mmap(NULL,4096,PROT_READ|PROT_WRITE,MAP_ANONYMOUS|MAP_SHARED,-1,0);
+	volatile uint32_t *step2;
+	{
+		if (step1 == ((uint32_t*)-1)) {
+			printf("Cannot mmap 4K\n");
+			return 1;
+		}
+		if (mlock((void*)step1,4096) < 0) {
+			printf("Cannot mlock page into memory\n");
+			return 1;
+		}
+		uint32_t step1_cpuaddr = (uint32_t)virt2phys((void*)step1);
+		if (step1_cpuaddr == ~0) {
+			printf("Cannot translate to physical CPU addr\n");
+			return 1;
+		}
+
+		printf("Step1 page:\n");
+		printf("    Virtual: 0x%08X\n",(uint32_t)step1);
+		printf("    Physical: 0x%08X\n",step1_cpuaddr);
+
+		/* use the MTRRs to make our page uncacheable to ensure that the damn processor
+		 * get the data out there the instant we write it. if we don't the processor will
+		 * take it's sweet lazy time and you'll see it in the form of a screen full of
+		 * garbage that slowly builds up over time (as the CPU flushes, of course!) */
+		struct mtrr_sentry se;
+		memset(&se,0,sizeof(se));
+		se.base = step1_cpuaddr;
+		se.size = 4096;
+		if (ioctl(mtrr_fd,MTRRIOC_ADD_ENTRY,&se) < 0) {
+			printf("Cannot add MTRR entry\n");
+			return 1;
+		}
+
+		int fb_pages = (1280*768*2)>>12,c;
+		for (page=0;page < fb_pages;page++)
+			step1[page] = (framebuffer_addr + (page << 12UL)) | 1;
+
+		/* and then some pages to allow us to access the VESA BIOS's table.
+		 * so we can rebuild it in case it got trashed. this makes soft-booting safe again too;
+		 * the VESA BIOS seems to assume it can build them once and lazily re-use them every
+		 * time Linux uvesafb calls INT 10H, therefore if the tables are trashed the VESA BIOS
+		 * will seriously fuck up the system trying to handle INT 10H and cause hang, crash,
+		 * reboot, or severe data corruption! */
+		uint32_t vesa_mapped_offset = page << 12UL;
+		for (c=0;c < (page_tables_size>>12);c++,page++)
+			step1[page] = (vesa_bios_page_tables + (c << 12UL)) | 1;
+		/* and the rest point nowhere */
+		for (;page < 1024;page++)
+			step1[page] = framebuffer_addr | 1;
+
+		if (ioctl(mtrr_fd,MTRRIOC_DEL_ENTRY,&se) < 0)
+			printf("Warning: cannot kill MTRR entry\n");
+
+		MMIO(0x2020) = step1_cpuaddr | 1;
+
+		/* and then, use the mapping we made for ourselves to access what the VESA BIOS uses for page tables */
+		step2 = (volatile uint32_t*)(fb_base + vesa_mapped_offset);
+	}
+
+	/* rebuild the VESA BIOS copy of page tables. mimic the mapping-out of the tables the VESA BIOS does. */
+	{
+		int page,pages = (page_tables_size >> 2);
+		int map_pages = (stolen - page_tables_size) >> 12;
+		for (page=0;page < map_pages;page++)
+			step2[page] = (framebuffer_addr + (page << 12UL)) | 1;
+		for (;page < pages;page++)
+			step2[page] = (framebuffer_addr + ((map_pages-1) << 12UL)) | 1;
+	}
+
+	MMIO(0x2020) = vesa_bios_page_tables | 1;
 
 	/* I pick the 2MB mark for the ring buffer. Use larger space for speed tests, about slightly less than 2MB */
 	set_ring_area(0x200000,2040*1024);
@@ -175,8 +247,8 @@ int main() {
 	}
 
 	stop_ring();
-	disable_gtt();
 	unmap_intel_resources();
+	close(mtrr_fd); mtrr_fd = -1;
 	return 0;
 }
 
